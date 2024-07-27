9b105de37b18780f16dc6670bd966a81a7c75480
Flavia Ferreira <flaviaferreiradev@gmail.com>
2024-07-27 13:32:52 -0300

    Apendendo na pratica conceitos de JavaScript

---
+++ programacaoComJavaScriptConceitos/main.js
@@ -0,0 +1,23 @@
+console.log('testando');
+var a = 10;
+var b = 20;
+var total = a + b;
+console.log(total);
+var nomePessoa = 'Lucas';
+console.log(nomePessoa);
+
+var var1 = 1,
+  var2 = 2,
+  var3 = 3;
+console.log(var3);
+var variavel1;
+variavel1 = 23;
+console.log(variavel1);
+
+let book = 10;
+book = 20;
+console.log(book);
+
+const valor1 = 100;
+valor1 = 200;
+console.log(valor1);
